5ee50701bdad24722ff6a79bbd282be77fe91efb
Mark Kenneth Ranosa <mark.ranosa@gmail.com>
2013-09-30 07:10:45 +0800

    Added in intial junit template

--- test/trains/TestTrains.java
+++ test/trains/TestTrains.java
@@ -1,5 +1,26 @@
 package trains;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Tests for {@link Foo}.
+ * 
+ * @author user@example.com (John Doe)
+ */
+@RunWith(JUnit4.class)
 public class TestTrains {
 
+	@Test
+	public void thisAlwaysPasses() {
+	}
+
+	@Test
+	@Ignore
+	public void thisIsIgnored() {
+	}
 }
